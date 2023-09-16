f21c15246d839e3da414f10f6690aad24724dc4c
Jeff <jeffreychen51@gmail.com>
2023-09-16 17:09:34 -0400

    image

--- database/driver.py
+++ database/driver.py
@@ -1,15 +1,25 @@
 import clothe_database as cd
-
+from PIL import Image
+import urllib.request
 cd.delete_clothes("./fashion.db")
 cd.init_clothes("./fashion.db")
 
 hoodie = cd.clothe('casual outer', 0, 255, 0, 20)
 shirt = cd.clothe('casual inner', 0, 0, 255, 18)
 pants = cd.clothe('casual pant', 25, 25, 25, 32)
-nice_pants = cd.clothe('formal pant', 100, 100, 100, 30)
+nice_pants = cd.clothe('formal pant', 100, 100, 100, 30, "https://media.istockphoto.com/id/1221134337/photo/front-views-black-trousers.jpg?s=612x612&w=0&k=20&c=XSEtHBJpWPQTo5FQgVTGrLBRiOJ7FuYIcUlKZzifGG8=")
 nice_shirt = cd.clothe('formal shirt', 0, 50, 150, 22)
+tee_shirt = cd.clothe('casual inner', 0, 200, 250, 20)
 
 cd.add_clothes('./fashion.db', hoodie)
 cd.add_inventory('./fashion.db', [hoodie, shirt, pants])
 cd.add_inventory('./fashion.db', [nice_pants, nice_shirt])
+cd.add_clothes('./fashion.db', tee_shirt)
 
+#print(cd.get_clothes('./fashion.db'))
+#print(cd.get_category('./fashion.db', 'casual inner'))
+#link = cd.get_id('./fashion.db', 5)[6]
+#print(link)
+
+link, img = cd.get_img('./fashion.db', 5)
+img.show()
